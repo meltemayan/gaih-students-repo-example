572cc854ccdf1f7354c4682d266d3be74d53ffd2
meltemayan <42765484+meltemayan@users.noreply.github.com>
2021-02-21 23:40:05 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,23 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+def primefirst(x):
+  for num in range(x,x + 1):
+    if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+        print(num) 
+          
+def primesecond(x):  
+  for num in range(x,x + 1):
+    if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+              break
+       else:
+         print(num)
+           
+for i in range(1000):
+  if i < 500:
+    primefirst(i)
+  else:
+    primesecond(i)
