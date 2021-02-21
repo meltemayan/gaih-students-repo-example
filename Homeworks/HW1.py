4e9f032582c7fc45032fe3e33b196597f06f2ee5
meltemayan <42765484+meltemayan@users.noreply.github.com>
2021-02-21 23:38:12 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,22 @@
-#Explain your work
+num1 = 1
+num2 = 100
+primes = []
 
-#Question 1
-for x in range(a):
-	print(a)
+for num in range(num1,num2 + 1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           primes.append(num)
+
+import random
+
+row = range(3)
+mat = []
+for i in range(3):
+  row = []
+  for j in range(3):
+    row.append(random.choice(primes))
+  mat.append(row)
+  print(mat[i])
